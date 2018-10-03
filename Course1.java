548d175f50dbd081622497ebd705446205bcd993
vijaygarg04 <vijaygarg04@gmail.com>
2018-10-03 13:18:42 +0530

    Rename Course.java to Course1.java

---
+++ Course1.java
@@ -0,0 +1,17 @@
+package com.example.shivam.adminapplication.Models;
+
+public class Course {
+    String coursename;
+
+    public Course(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+}
